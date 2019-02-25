97839621915f67efb24491704af8a50095963918
USER <a01700585@itesm.mx>
2019-02-25 17:52:24 -0600

    Lab 3 segunda parte

---
+++ Lab3/app/src/main/java/mx/tec/lab3/ChildActivity.java
@@ -0,0 +1,23 @@
+package mx.tec.lab3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        Intent targetIntent;
+        targetIntent = getIntent();
+        String myText;
+        myText = targetIntent.getStringExtra("myData");
+        TextView myTextView;
+        myTextView = (TextView)findViewById(R.id.textView2);
+        myTextView.setText(myText);
+
+    }
+}
--- Lab3/app/src/main/java/mx/tec/lab3/MainActivity.java
+++ Lab3/app/src/main/java/mx/tec/lab3/MainActivity.java
@@ -1,8 +1,13 @@
 package mx.tec.lab3;
 
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -49,5 +54,47 @@ public class MainActivity extends AppCompatActivity {
 
         AlertDialog MyDialog = DialogConf.create();
         MyDialog.show();
+
+
+        // Notifications
+        AlertDialog.Builder ThirdDialogConf = new AlertDialog.Builder(this);
+
+        ThirdDialogConf.setTitle("Confirmation Dialog");
+        ThirdDialogConf.setMessage("This is the Text of the Dialog");
+        ThirdDialogConf.setIcon(R.mipmap.ic_launcher);
+
+        final EditText TextInput = new EditText(context);
+        TextInput.setTextColor(Color.BLUE);
+        ThirdDialogConf.setView(TextInput);
+
+
+        ThirdDialogConf.setNeutralButton("Close",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        String str = TextInput.getEditableText().toString();
+                        Toast.makeText(context, "You wrote this in the dialog: " + str, Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        AlertDialog MyThirdDialog = ThirdDialogConf.create();
+        MyThirdDialog.show();
+
+
+        /*Step 5: Bar Notification ---------------------------------------------------------------*/
+
+        int NOTIF_ID = 1234;
+        Notification.Builder NotifBuilder = new Notification.Builder(this);
+        NotifBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        NotifBuilder.setContentTitle("Important Notification");
+        NotifBuilder.setContentText("This is the detail of the notification");
+
+        Intent notificationIntent = new Intent(context, ChildActivity.class);
+        notificationIntent.putExtra("myData", "This string comes from the previous activity");
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+        NotifBuilder.setContentIntent(contentIntent);
+
+        NotificationManager MyNotification = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        MyNotification.notify(NOTIF_ID, NotifBuilder.build());
     }
 }
